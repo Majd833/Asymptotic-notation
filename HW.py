e284dde49cf7ae9d5ea73bb854e44d500f8b06af
Majd833 <lina.azzam.1611@gmail.com>
2025-02-11 17:08:18 +0200

    HW

---
+++ HW.py
@@ -0,0 +1,11 @@
+def myfun(n):
+    for i in range(0,n+1):
+        print("first loop")
+    j=1
+    while(j<=n+1):
+        print("second loop")
+        j=j*2
+    for i in range(0,100):
+        print(" third loop")
+print(" the above function takes time")
+print
